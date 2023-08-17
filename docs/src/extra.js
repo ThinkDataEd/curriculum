86a9368155c72bfea5f4bf8b1219f8244b5896d0
sw <sw3023@columbia.edu>
2023-08-16 23:02:44 -0700

    Update extra.js

--- docs/src/extra.js
+++ docs/src/extra.js
@@ -1,3 +1,6 @@
+
+
+
 function setCookie20230723(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -35,17 +38,24 @@ function additionalCheck() {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         // console.log(xhr.responseText);
-        var resp = xhr.responseText;
-        resp = resp.replace(/"/g, "");
-        if (xhr.responseText.includes("success") && resp.includes("can_create_classes:true")) {
+        var data = JSON.parse(xhr.responseText).data; 
+        if (xhr.responseText.includes("success") && Object.values(data).some((dat)=>dat?.permissions?.can_create_classes)) {
+          document.body.style.display = 'block';
           console.log("PASSED");
         } else {
+          document.body.style.display = 'none';
           window.location.href = window.location.origin + "/";
         }
       } else {
+        document.body.style.display = 'none';
         window.location.href = window.location.origin + "/";
       }
     }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+    document.body.style.display = 'none';
+    window.location.href = window.location.origin + "/";
   };
   xhr.send(params);
 }
@@ -98,17 +108,21 @@ if (window.location == window.top.location) {
       if (xhr.status === 200) {
         // console.log(xhr.responseText);
         if (xhr.responseText.includes("success")) {
+          // document.body.style.display = 'none';
           additionalCheck();
         } else {
+          document.body.style.display = 'none';
           window.location.href = window.location.origin + "/#login";
         }
       } else {
+        document.body.style.display = 'none';
         window.location.href = window.location.origin + "/#login";
       }
     }
   };
   xhr.onerror = function (e) {
     console.error(xhr.statusText);
+    document.body.style.display = 'none';
     window.location.href = window.location.origin + "/#login";
   };
   xhr.send(null);
